7573f704efa557d0e8a0afa2b9de244fafd4c28a
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-03-05 18:30:03 +0530

    plis work on sizes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,7 +20,7 @@ function Projects({}: Props) {
       className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly items-center mx-auto z-0"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl">
-        Skills
+        Projects
       </h3>
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project) => (
