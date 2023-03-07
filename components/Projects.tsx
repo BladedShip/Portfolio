0c57efc1642056659812ed1ac7dbb1776eba893d
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-03-07 12:45:33 +0530

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -26,20 +26,20 @@ function Projects({}: Props) {
         {projects.map((project) => (
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p:20 md:p-44 h-screen">
             <motion.img
-            initial={{
-                y:-300,
-                opacity:0,
-            }}
-            transition={{
-                duration:1,
-            }}
-            whileInView={{
-                opacity:1,
-                y:0,
-            }}
-            viewport={{
-                once:true,
-            }}
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{
+                duration: 1,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              viewport={{
+                once: true,
+              }}
               src="https://cdn.worldvectorlogo.com/logos/react-2.svg"
               alt="React"
               className="w-40"
